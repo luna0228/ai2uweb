0fbc53aff6e5567a856cd5de034085b2e2b943b6
luna0228 <51066795+luna0228@users.noreply.github.com>
2025-02-03 11:27:14 +0800

    update 觀看課程sidebar更新

--- assets/js/ui_setting.js
+++ assets/js/ui_setting.js
@@ -18,5 +18,41 @@
                 form.classList.add('was-validated')
             }, false)
         })
+
+
 })()
+
+//open\close sidebar
+var sidebarSwitchHide = document.getElementById('sidebar-hide')
+var sidebarSwitchOpen = document.getElementById('sidebar-open')
+var sidebarContent = document.getElementById('sidebar-content')
+var mainContent = document.getElementById('main-content')
+// console.log(sidebarSwitch)
+// var bsButton = new bootstrap.Button(sidebarSwitchHide)
+// var bsButton = new bootstrap.Button(sidebarSwitchOpen)
+sidebarSwitchHide.onclick = function () {
+    if (!sidebarContent.classList.contains("close") && !mainContent.classList.contains("full")) {
+        sidebarContent.classList.add("close");
+        mainContent.classList.add("full");
+        console.log('1')
+    }
+    else {
+        sidebarContent.classList.remove("close");
+        mainContent.classList.remove("full");
+        console.log('2')
+    }
+};
+sidebarSwitchOpen.onclick = function () {
+    if (!sidebarContent.classList.contains("close") && !mainContent.classList.contains("full")) {
+        sidebarContent.classList.add("close");
+        mainContent.classList.add("full");
+        console.log('1')
+    }
+    else {
+        sidebarContent.classList.remove("close");
+        mainContent.classList.remove("full");
+        console.log('2')
+    }
+};
+
 
