c956637eba1fdfffb5d842aee2cf8910c9738d9f
luna0228 <51066795+luna0228@users.noreply.github.com>
2025-02-14 17:12:51 +0800

    update 小版顯示、調整效果

--- assets/js/ui_setting.js
+++ assets/js/ui_setting.js
@@ -19,10 +19,27 @@
             }, false)
         })
 
-
 })()
 
+//mobile
+window.onload = function () {
+    function resize() {
+        console.log('resize')
+        let w_width = document.documentElement.clientWidth
+        if (w_width < 1024) {
+            console.log('<1024')
+
+        }
+        else {
+            console.log('>1024')
+        }
+    }
+    resize()
+    window.addEventListener('resize', resize, false)	// 偵聽事件 resize
+}
+
 //open\close sidebar
+
 var sidebarSwitchHide = document.getElementById('sidebar-hide')
 var sidebarSwitchOpen = document.getElementById('sidebar-open')
 var sidebarContent = document.getElementById('sidebar-content')
@@ -30,29 +47,38 @@ var mainContent = document.getElementById('main-content')
 // console.log(sidebarSwitch)
 // var bsButton = new bootstrap.Button(sidebarSwitchHide)
 // var bsButton = new bootstrap.Button(sidebarSwitchOpen)
-sidebarSwitchHide.onclick = function () {
-    if (!sidebarContent.classList.contains("close") && !mainContent.classList.contains("full")) {
-        sidebarContent.classList.add("close");
-        mainContent.classList.add("full");
-        console.log('1')
-    }
-    else {
-        sidebarContent.classList.remove("close");
-        mainContent.classList.remove("full");
-        console.log('2')
-    }
-};
-sidebarSwitchOpen.onclick = function () {
-    if (!sidebarContent.classList.contains("close") && !mainContent.classList.contains("full")) {
-        sidebarContent.classList.add("close");
-        mainContent.classList.add("full");
-        console.log('1')
-    }
-    else {
-        sidebarContent.classList.remove("close");
-        mainContent.classList.remove("full");
-        console.log('2')
-    }
-};
+if (sidebarSwitchHide) {
+    sidebarSwitchHide.onclick = function () {
+        if (!sidebarContent.classList.contains("close") && !mainContent.classList.contains("full")) {
+            sidebarContent.classList.add("close");
+            mainContent.classList.add("full");
+            sidebarSwitchOpen.classList.add("show");
+            console.log('1')
+        }
+        else {
+            sidebarContent.classList.remove("close");
+            mainContent.classList.remove("full");
+            sidebarSwitchOpen.classList.remove("show");
+            console.log('2')
+        }
+    };
+}
+if (sidebarSwitchOpen) {
+    sidebarSwitchOpen.onclick = function () {
+        if (!sidebarContent.classList.contains("close") && !mainContent.classList.contains("full")) {
+            sidebarContent.classList.add("close");
+            mainContent.classList.add("full");
+            sidebarSwitchOpen.classList.add("show");
+            console.log('1')
+        }
+        else {
+            sidebarContent.classList.remove("close");
+            mainContent.classList.remove("full");
+            sidebarSwitchOpen.classList.remove("show");
+            console.log('2')
+        }
+    };
+}
+
 
 
